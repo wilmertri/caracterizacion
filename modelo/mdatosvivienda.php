b48c569b1af1b99b686ab62ad76ee8cc09df11b2
Wilmer Triana <fabiantriana1072@gmail.com>
2015-03-29 10:53:32 -0500

    Agregar modelo datosvivienda

--- modelo/mdatosvivienda.php
+++ modelo/mdatosvivienda.php
@@ -1,4 +1,8 @@
 <?php
+	/*
+		@author: Wilmer Fabian Triana
+		@Version: V1
+	*/
 	include ("controlador/conexion.php");
 	class Mdatosvivienda {
 		function Mdatosvivienda()
@@ -6,10 +10,22 @@
 
 		}
 		
+		/*
+			Función para la actualización de los datos de la vivienda de la persona
+		*/
 		function actu_datos_vivienda($tipovivienda, $tipoparedes, $tipopisos, $tipotechos, $zonariesgo, $idpersona)
 		{
 			$sql = "UPDATE tbdatospersona SET tipvivper=$tipovivienda, tipparviv=$tipoparedes, tippisviv=$tipopisos, tiptecviv=$tipotechos, zonrieviv=$zonariesgo WHERE idpersona = $idpersona";
 			$this->cons($sql);
+		}
+
+		function ver_datos_vivienda($idpersona)
+		{
+			$sql = "SELECT tipvivper, tipparviv, tippisviv, tiptecviv, zonrieviv FROM tbdatospersona WHERE idpersona = $idpersona";
+			$conexionBD = new conexion();
+			$conexionBD->conectarBD();
+			$datos = $conexionBD->ejeCon($sql,0);
+			return $datos;
 		}
 
 		function cons ($sentencia_sql) 
