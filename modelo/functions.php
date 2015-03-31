6931477794067f2f1cbe275321802133121a9dde
Wilmer Triana <fabiantriana1072@gmail.com>
2015-03-31 11:55:52 -0500

    Agregación de función para hallar la edad
    Se realiza la función para hallar la edad según la fecha de nacimiento.
    Se agrega al archivo functions.

--- modelo/functions.php
+++ modelo/functions.php
@@ -1,8 +1,17 @@
 <?php
+		/*
+		*	@author: Wilmer Fabian Triana
+		*	@Version: V1
+		*/		
 		function functions()
 		{
 
 		}
+
+		/*
+		*	Función para seleccionar los valores de un determinado parametro
+		*/
+
 		function seleccionar_valores_de_parametro($codpar)
 		{
 			$sql = "SELECT * FROM tbvalor WHERE codpar = $codpar";
@@ -11,6 +20,10 @@
 			$datos = $conexionBD->ejeCon($sql,0);
 			return $datos;
 		}
+
+		/*
+		*	Función para mostrar los nombres de determinado valor
+		*/
 
 		function mostrar_nombre_valores($codval)
 		{
@@ -21,4 +34,16 @@
 			return $datos;
 		}
 
+		/*
+		*	Función para calcular la edad según la fecha de nacimiento
+		*/
+
+		function calcular_edad($fecha)
+		{
+			$dias = explode("-", $fecha, 3);
+			$dias = mktime(0,0,0,$dias[1],$dias[2],$dias[0]);
+			echo "Dias: " . $dias;
+			$edad = (int)((time()-$dias)/31556926 );
+			return $edad;
+		}
  ?>
