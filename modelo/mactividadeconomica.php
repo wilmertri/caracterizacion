2eb3adb1fbaf66a211acec1e15d784df8a17d088
Diego Felipe Monroy <difemo91@gmail.com>
2015-05-13 22:21:34 -0500

    Modificacion MVC de actividad economica
    Se creo el modelo de actividad economica y se crearon las variables para
    el ingreso de los datos

---
+++ modelo/mactividadeconomica.php
@@ -0,0 +1,64 @@
+<?php
+	/*
+	 *	@author: 
+	 *	@Version: 
+	 */
+	include ("controlador/conexion.php");
+	include("functions.php");
+	class Mactividadeconomica extends Funciones_generales {
+            
+                
+		function Mactividadeconomica()
+		{
+                    
+		}
+
+        /*
+		 *	Función para obetner los nombres de los valores	
+	     */ 
+
+        function get_valor($codval)
+		{
+			return $this->mostrar_nombre_valores($codval);
+		}
+                        
+        /*
+		 *	Función para obtener la ocupacion actual
+	     */
+	                     
+		function get_ocupacion()
+		{
+			return $this->seleccionar_valores_de_parametro(31);
+		}
+
+		/*
+		 *	Función para obtener los tipos de pisos	
+	     */
+		
+		/*function get_tipo_pisos()
+		{
+			return $this->seleccionar_valores_de_parametro(17);
+		}
+	    */
+
+		/*
+		 *	Función para la actualización de los datos de la actividad economica de la persona
+		 */
+		function actu_datos_actividad($numpercar, $ocuperact, $trabper, $ingmenper, $idpersona)
+		{
+			$sql = "UPDATE tbdatospersona SET numpercar='$numpercar', ocuperact='$ocuperact', actperact='$trabper', ingmenper='$ingmenper' WHERE idpersona = '$idpersona'";
+			$this->cons($sql);
+		}
+
+        /*
+    	 *	Función para retornar los datos de la actividad economica	
+         */
+
+        function ver_datos_actividad($idpersona)
+		{
+			$sql = "SELECT numpercar, ocuperact, actperact, ingmenper FROM tbdatospersona WHERE idpersona = $idpersona";			
+            return $this->SeleccionDatos($sql);
+		}
+		
+	} 
+?>
--- vista/actividadeconomica/vactividadeconomica.php
+++ vista/actividadeconomica/vactividadeconomica.php
@@ -1,5 +1,5 @@
 <?php
-	//include("controlador/"...." .php");
+	include("controlador/cactividadeconomica.php");
 ?>
 <h1>Actividad Economica</h1>
 <div class="forms1">
@@ -7,51 +7,62 @@
         <div class="row">
             <div class="form-group col-lg-6">
                 <label for="Cuantas personas tiene a su cargo ">Cuantas personas tiene a su cargo</label>	
-                <input name="numpercar" type="text" class="form-control" min="0" max="30"> <!--esta variable viene de la tabla vdatospersona-->
+                <input name="numpercar" type="number" class="form-control" pattern="[0-9]{1,3}" min="0" max="30" value="<?= $datosactividad[0]['numpercar'] ?>"> <!--esta variable viene de la tabla vdatospersona-->
             </div>
 			<div class="form-group col-lg-6">
                 <label for="Ocupación Actual">Ocupación Actual</label>
-                <input name="ocuperact" type="text" class="form-control" min="0" max="30">
-            </div>
+				<select name="ocuperact" class="form-control" id="">
+					<option value="<?= $datosactividad[0]['ocuperact'] ?>">Seleccione una opción</option>
+					<?php
+
+						for ($i=0; $i <count($ocuperact1) ; $i++) 
+						{  
+					?>
+					<option value="<?php echo $ocuperact1[$i]['codval']; ?>"><?php echo $ocuperact1[$i]['nomval']; ?></option>
+					<?php 
+						} 
+					?>
+				</select>		
+			</div>
 		</div>
 		<div class="row">	
 			<div class="form-group col-lg-6">
                 <label for="En que trabaja">En que trabaja</label>
-                <input name="actperact" type="text" class="form-control" min="0" max="30">
+                <input name="trabper" type="text" class="form-control" placeholder="" pattern="[A-z ]{2,20}" >
             </div>
 			<div class="form-group col-lg-6">
                 <label for="Ingresos Mensuales">Ingresos Mensuales</label>
-                <input name="" type="text" class="form-control" min="0" max="30"> <!--falta completar ya que estos datos vienen tb-ingresos-->
+                <input name="ingmenper" type="text" class="form-control" placeholder="ej: 680000" pattern="[0-9]{1,10}" value="<?= $datosactividad[0]['ingmenper'] ?>">
             </div>
 		</div>
-		<div class="row">	
+		<!--<div class="row">	
 			<div class="form-group col-lg-6">
                 <label for="Fuente de los Ingresos">Fuente de los Ingresos</label>
-                <input name="ingresosn" type="text" class="form-control" min="0" max="30"> <!--nota: Fuente - crear Fuente ingresosn ext,- fuenteing 4 campos (cod, tiping, ing, docper)-->
+                <input name="ingresosn" type="text" class="form-control" min="0" max="30"> 
             </div>
 			<div class="form-group col-lg-6">
                 <label for="Otro Cual?">Otro Cual?</label>
-                <input name="" type="text" class="form-control" min="0" max="70"> <!--falta completar para saber que se va  colocar en esta opcion-->
+                <input name="" type="text" class="form-control" min="0" max="70"> <!--falta completar para saber que se va  colocar en esta opcion
             </div>
 		</div>	
 		<div class="row">	
 			<div class="form-group col-lg-6">
                 <label for="Recibe ayuda a traves de alguno de los siguientes programas sociales?">Recibe ayuda a traves de alguno de los siguientes programas sociales?</label>
-                <input name="" type="text" class="form-control" min="0" max="30"> <!--fuente ingresos externa-->
+                <input name="" type="text" class="form-control" min="0" max="30"> <!--fuente ingresos externa
             </div>
 			<div class="form-group col-lg-6">
                 <label for="Oficios en los que tiene experiencia laboral">Oficios en los que tiene experiencia laboral</label>
-                <input name="" type="text" class="form-control" min="0" max="30"> <!-- completar tb exp. lab-->
+                <input name="" type="text" class="form-control" min="0" max="30"> <!-- completar tb exp. lab
             </div>
 		</div>
 		<div class="row">	
 			<div class="form-group col-lg-6">
                 <label for="Ha emprendido algún Negocio?">Ha emprendido algún Negocio?</label>
-                <input name="" type="text" class="form-control" min="0" max="30"> <!--comletar tbnegocio vienen los datos-->
+                <input name="" type="text" class="form-control" min="0" max="30"> <!--comletar tbnegocio vienen los datos
             </div>
 			<div class="form-group col-lg-6">
                 <label for="¿Qué Tipo de Negocio es?">¿Qué Tipo de Negocio es?</label>
-                <input name="" type="text" class="form-control" min="0" max="30"> <!--completar tb negocio revisar-->
+                <input name="" type="text" class="form-control" min="0" max="30"> <!--completar tb negocio revisar
             </div>
 		</div>
 		<div class="row">	
@@ -61,30 +72,32 @@
             </div>
 			<div class="form-group col-lg-6">
                 <label for="Duración del Negocio">Duración del Negocio</label>
-                <input name="" type="text" class="form-control" min="0" max="30"> <!--agregar despregable-->
+                <input name="" type="text" class="form-control" min="0" max="30"> <!--agregar despregable
             </div>
-		</div>	
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Enviar">
-				<a href="home.php" class="btn btn-primary" >Salir</a>
+		</div>	-->
+		<div class="row">
+            <div class="form-group col-lg-12 center">
+                <input type="submit" class="btn btn-success" value="Enviar">
+				<a href="javascript:history.back();" class="btn btn-success">Volver</a>
+				<a href="home.php" class="btn btn-success" >Salir</a>
             </div>
-        </div>
+		</div>
     </form>
-	</div>
+</div>
 
 	<?php
 		//$dato= $ins->selcap1($pr);  // MODIFICAR
 		//for($i=0;$i<count($dato);$i++)
-			{
+			//{
 	?>
-		<tr>
-			<td><//?php  echo $dato[$i]['codcapper']?></td> <!-- MODIFICAR EL TR-->
+		<!--<tr>
+			<td><//?php  echo $dato[$i]['codcapper']?></td>  MODIFICAR EL TR
 			<td><//?php echo $dato[$i]['nomcap']?></td>
 		    <td><//?php echo $dato[$i]['entcap']?></td>
             <td><//?php echo $dato[$i]['titcap'] ?></td>
-            <td><a href="home.php?var=11&pr=<?php echo $dato1[0]['numdocper'] ?>&cod=<?php echo $dato[$i]["codcapper"]; ?>" class="btn btn-primary">Editar</a></td>
+            <td><a href="home.php?var=11&pr<?php //echo $dato1[0]['numdocper'] ?>&cod=<?php //echo $dato[$i]["codcapper"]; ?>" class="btn btn-primary">Editar</a></td>
         </tr>
-	<?php } ?>		
+	<?php //} ?>		
 		</table>
 	</form>
-</div>
+</div>-->
